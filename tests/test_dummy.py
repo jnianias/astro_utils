626b3965f349f305b3f1449e5815aa07c35dc673
jnianias <jnianias@gmail.com>
2026-01-12 09:49:30 +0800

    added a dummy test for the CI pipeline

---
+++ tests/test_dummy.py
@@ -0,0 +1,7 @@
+"""
+Dummy test to verify that the CI pipeline is running correctly.
+This test always passes and can be removed or replaced with real tests later.
+"""
+
+def test_dummy():
+    assert True
