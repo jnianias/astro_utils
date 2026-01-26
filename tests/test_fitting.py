6a623294328a8195e67ed479d09da6936329b204
jnianias <jnianias@gmail.com>
2026-01-26 16:37:53 +0800

    refactored routines to generate initial guesses and bounds

--- tests/test_fitting.py
+++ tests/test_fitting.py
@@ -26,7 +26,7 @@ def test_get_initial_guesses_from_catalog():
 
     expected_guesses = {
         'AMPB'   :  380.14799202510267,
-        'LPEAKB' :  5152.057737999559 - 7.5,
+        'LPEAKB' :  5152.057737999559 - 2 * (1 + (5152.057737999559 / wavedict['LYALPHA'] - 1)),
         'DISPB'  :  3.9649905781616326 / 2.355,
         'ASYMB'  :  -0.1,
         'AMPR'   :  380.14799202510267,
@@ -38,26 +38,44 @@ def test_get_initial_guesses_from_catalog():
 
     assert initial_guesses == pytest.approx(expected_guesses, rel=1e-5)
 
-# def test_gen_bounds():
-#     """
-#     Test the gen_bounds function to ensure it generates correct parameter bounds.
-#     """
+def test_gen_bounds():
+    """
+    Test the gen_bounds function to ensure it generates correct parameter bounds.
+    """
 
-#     # Test case 1: automatic bounds for lLyman alpha
-#     redshift = 4.0
-#     initial_guesses = {
-#         'FLUXB' :  
-#         'LPEAKB': wavedict['LYALPHA'] * (1 + redshift) - 8,
-#         '
-#     }
-#     expected_bounds = [(0.0, 10.0), (1.0, 5.0)]
-#     bounds = aufit.gen_bounds(param_names, lower_limits, upper_limits)
-#     assert bounds == expected_bounds
+    full_iden = 'P594'
+    cluster   = 'MACS0416NE'
+    line_name = 'LYALPHA'
 
-#     # Test case 2: curstom bounds
-#     param_names = ['paramA', 'paramB']
-#     lower_limits = [-5.0, -10.0]
-#     upper_limits = [0.0, 0.0]
-#     expected_bounds = [(-5.0, 0.0), (-10.0, 0.0)]
-#     bounds = aufit.gen_bounds(param_names, lower_limits, upper_limits)
-#     assert bounds == expected_bounds
+    # Retrieve line table from catalog
+    line_table = aucat.get_muse_cand(full_iden, cluster)
+
+    # Test case 1: automatic bounds for Lyman alpha
+    initial_guesses = aufit.get_initial_guesses_from_catalog(line_table, line_name, type='em')
+
+    z = ( initial_guesses['LPEAKR'] / wavedict['LYALPHA'] ) - 1
+
+    lpeakb_tol = [-3 * (1 + z), -1 * (1 + z)]
+    lpeakr_tol = [-1 * (1 + z), 3 * (1 + z)]
+
+    expected_bounds = {
+        'AMPB'   : (0, 10000),
+        'LPEAKB' : (initial_guesses['LPEAKR'] + lpeakb_tol[0], 
+                    initial_guesses['LPEAKR'] + lpeakb_tol[1]),
+        'DISPB'  : (0.2 * (1 + z), 1 * (1 + z)),
+        'ASYMB'  : (-0.5, 0.5),
+        'AMPR'   : (0, 10000),
+        'LPEAKR' : (initial_guesses['LPEAKR'] + lpeakr_tol[0], 
+                    initial_guesses['LPEAKR'] + lpeakr_tol[1]),
+        'DISPR'  : (0.2 * (1 + z), 1 * (1 + z)),
+        'ASYMR'  : (-0.5, 0.5),
+        'CONT'   : (-50, 2000)
+    }
+    
+    bounds = aufit.gen_bounds(initial_guesses, line_name, force_sign='positive')
+
+    # Check keys are the same
+    assert set(bounds.keys()) == set(expected_bounds.keys())
+    # Check values are approximately equal
+    for key in expected_bounds:
+        assert bounds[key] == pytest.approx(expected_bounds[key], rel=1e-5)
